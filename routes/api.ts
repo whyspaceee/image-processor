7bbae9950305566a05a3ff9ecbe9c6e45faaf21f
Almas Mirzandi <almasmirzandir@gmail.com>
2023-05-29 19:12:56 +0700

    add compress controller

--- routes/api.ts
+++ routes/api.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { cropImage, cropUpload } from "../controllers/CropController";
 import { convertImage, convertUpload } from "../controllers/ConvertController";
 import { getS3Link, multerUpload } from  "../controllers/UploadController";
+import { compressJPEG, compressUpload } from "../controllers/CompressController";
 
 const api = Router();
 
 api.post("/upload", multerUpload, getS3Link, )
 api.post("/crop", cropUpload, cropImage )
 api.post("/convert", convertUpload, convertImage )
+api.post("/compress", compressUpload, compressJPEG )
 
 api.get("/", (req, res) => {
     res.send("Hello World!");
