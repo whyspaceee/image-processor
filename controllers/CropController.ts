edb3143e59005a5b52f5edfc788b715daf7085d1
whyspaceee <mohammad.abdan.syakura@mail.ugm.ac.id>
2023-06-06 23:56:07 +0700

    bug fix

--- controllers/CropController.ts
+++ controllers/CropController.ts
@@ -5,66 +5,74 @@ import sharp from "sharp";
 import { s3 } from "../utils/aws";
 import { streamToBuffer } from "../utils/streamToBuffer";
 
-export const cropImage = async (req: Request, res: Response) => {
-  const { imageId } = req.params; // Extract the imageId parameter from the request URL
+export const cropImage = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { imageId } = req.params; // Extract the imageId parameter from the request URL
 
-  const { width, height } = req.query;
+    const { width, height } = req.query;
 
-  const top = req.query.top || "0";
-  const left = req.query.left || "0";
+    const top = req.query.top || "0";
+    const left = req.query.left || "0";
 
-  if (!imageId) {
-    return res.status(400).json({ error: "Missing imageid" });
+    if (!imageId) {
+      return res.status(400).json({ error: "Missing imageid" });
+    }
+
+    if (!width || !height) {
+      return res.status(400).json({ error: "Missing crop parameters" });
+    }
+
+    const crop = {
+      left: parseInt(left.toString()),
+      top: parseInt(top.toString()),
+      width: parseInt(width.toString()),
+      height: parseInt(height.toString()),
+    };
+
+    const object = await s3.send(
+      new GetObjectCommand({
+        Key: imageId,
+        Bucket: "gama-scalable",
+      })
+    );
+
+    if (!object.Body) {
+      return res.status(400).json({ error: "image not found" });
+    }
+
+    const buffer = await streamToBuffer(object.Body);
+
+    const metadata = await sharp(buffer).metadata();
+
+    const originalname = imageId.split("_").slice(1).join("_");
+
+    const key = `crop/${Date.now()}_${originalname}`;
+
+    const croppedBuffer = await sharp(buffer).extract(crop).toBuffer();
+
+    if (!croppedBuffer) {
+      return res.status(400).json({ error: "unable to crop image" });
+    }
+
+    await s3.send(
+      new PutObjectCommand({
+        Key: key,
+        Bucket: "gama-scalable",
+        Body: croppedBuffer,
+        ACL: "public-read",
+        ContentType: metadata.format,
+      })
+    );
+
+    return res.json({
+      message: "File cropped successfully",
+      url: `https://gama-scalable.s3.ap-southeast-1.amazonaws.com/${key}`,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  if (!width || !height) {
-    return res.status(400).json({ error: "Missing crop parameters" });
-  }
-
-  const crop = {
-    left: parseInt(left.toString()),
-    top: parseInt(top.toString()),
-    width: parseInt(width.toString()),
-    height: parseInt(height.toString()),
-  };
-
-  const object = await s3.send(
-    new GetObjectCommand({
-      Key: imageId,
-      Bucket: "gama-scalable",
-    })
-  );
-
-  if (!object.Body) {
-    return res.status(400).json({ error: "image not found" });
-  }
-
-  const buffer = await streamToBuffer(object.Body);
-
-  const metadata = await sharp(buffer).metadata();
-
-  const originalname = imageId.split("_").slice(1).join("_");
-
-  const key = `crop/${Date.now()}_${originalname}`;
-
-  const croppedBuffer = await sharp(buffer).extract(crop).toBuffer();
-
-  if (!croppedBuffer) {
-    return res.status(400).json({ error: "unable to crop image" });
-  }
-
-  await s3.send(
-    new PutObjectCommand({
-      Key: key,
-      Bucket: "gama-scalable",
-      Body: croppedBuffer,
-      ACL: "public-read",
-      ContentType: metadata.format,
-    })
-  );
-
-  return res.json({
-    message: "File cropped successfully",
-    url: `https://gama-scalable.s3.ap-southeast-1.amazonaws.com/${key}`,
-  });
 };
